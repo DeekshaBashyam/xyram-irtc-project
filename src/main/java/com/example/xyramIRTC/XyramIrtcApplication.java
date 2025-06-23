9c97c02aca110a3e747e63f3003cf52ceae7d7e2
DeekshaBashyam <b.deeksha@xyramsoft.com>
2025-06-23 18:30:16 +0530

    Initial commit - Added Spring Boot project

---
+++ src/main/java/com/example/xyramIRTC/XyramIrtcApplication.java
@@ -0,0 +1,13 @@
+package com.example.xyramIRTC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyramIrtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyramIrtcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/xyramIRTC/XyramIrtcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xyramIRTC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XyramIrtcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
