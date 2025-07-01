9d96df658f64e277d33e1286900dc5bc9b92f6f9
DeekshaBashyam <b.deeksha@xyramsoft.com>
2025-07-01 13:03:32 +0530

    added request mapping

--- src/main/java/com/example/xyramIRTC/controllers/TrainController.java
+++ src/main/java/com/example/xyramIRTC/controllers/TrainController.java
@@ -1,9 +1,13 @@
 package com.example.xyramIRTC.controllers;
 
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/trains")
 
 public class TrainController {
+
+
 }
